cd879a11d19a49899b7618dc2bb29af17e27ea5a
Kabirov Elmurod <182393579+KabirovElmurod@users.noreply.github.com>
2025-12-13 00:59:30 +0500

    kabirovelmurod

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'simple_user/home.html')
+
+def login(request):
+    return render(request, 'simple_user/login.html')
+def register(request):
+    return render(request, 'simple_user/register.html')
